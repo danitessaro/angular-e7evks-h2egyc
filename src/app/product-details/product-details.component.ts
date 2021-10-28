d55e19e67c6801540e604b73997ddbedbe6dbb26
danitessaro <danitcain@gmail.com>
2021-10-28 16:56:24 -0300

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -9,17 +9,16 @@ import { CartService } from '../cart.service';
   styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
   product: Product | undefined;
 
   addToCart(product: Product) {
     this.cartService.addToCart(product);
     window.alert('Your product has been added to the cart!');
   }
-  constructor(
-    private route: ActivatedRoute,
-    private cartService: CartService
-  ) {}
-
   ngOnInit() {
     // First get the product id from the current route.
     const routeParams = this.route.snapshot.paramMap;
